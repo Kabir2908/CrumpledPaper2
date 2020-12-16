0427fdb533138a817ee199ba992ec67eb102949b
Kabir2908 <68494610+Kabir2908@users.noreply.github.com>
2020-12-16 14:26:05 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -44,7 +44,7 @@ function draw() {
 }
 function keyPressed(){
 	if (keyCode=== UP_ARROW){
-		Matter.Body.applyForce(paper.body,paper.body.position,{x:35,y:-100});
+		Matter.Body.applyForce(paper.body,paper.body.position,{x:35,y:-75});
 	}
 }
 
